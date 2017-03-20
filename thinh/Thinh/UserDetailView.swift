707d9e4cd22eec8e7253d8ebeafbaf3a0f26952d
lelinh <lemanhlinhbk@gmail.com>
2017-03-20 23:33:45 +0700

    add user detail view

---
+++ thinh/Thinh/UserDetailView.swift
@@ -0,0 +1,36 @@
+//
+//  UserDetailView.swift
+//  Thinh
+//
+//  Created by Linh Le on 3/20/17.
+//  Copyright © 2017 coderschool. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailView: UIView {
+
+    @IBOutlet weak var UserBackgroundImage: UIImageView!
+    @IBOutlet weak var AvatarImage: UIImageView!
+
+    @IBOutlet weak var UserNameLabel: UILabel!
+    @IBOutlet weak var UserCaptionLabel: UILabel!
+    @IBOutlet weak var UserInfoLabel: UILabel!
+    
+    
+    
+    
+    @IBAction func onClickThaThinhButton(_ sender: UIButton) {
+    }
+    
+    
+    
+    /*
+     // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
