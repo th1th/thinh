bb0a6fd125e7abe4079ae561340f1b42674dccec
Dang Viet <dbviet@fossil.com>
2017-03-23 10:30:39 +0700

    Update Conversation View Controller

---
+++ thinh/Thinh/ConversationViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  ConversationViewCell.swift
+//  Thinh
+//
+//  Created by Viet Dang Ba on 3/23/17.
+//  Copyright © 2017 coderschool. All rights reserved.
+//
+
+import UIKit
+
+class ConversationViewCell: UITableViewCell {
+
+    @IBOutlet weak var partnerImg: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var lastMessageLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var thinhImg: UIImageView!
+    
+    var conversation : Conversation? {
+        didSet{
+            partnerImg.setImageWith((conversation?.partnerAvatar)!)
+            nameLabel.text = conversation?.partnerName
+            lastMessageLabel.text = conversation?.lastMessage
+            
+            // Handle time interval
+            //timeLabel.text = conversation?.lastTime
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- thinh/Thinh/ConversationViewController.swift
+++ thinh/Thinh/ConversationViewController.swift
@@ -10,26 +10,40 @@ import UIKit
 
 class ConversationViewController: UIViewController {
 
+    @IBOutlet weak var conversationTable: UITableView!
+    var conversationList : [Conversation]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let disposable = Api.shared().getAllConversation().subscribe(onNext: { (conversations) in
+            self.conversationList = conversations
+            self.conversationTable.reloadData()
+        })
+        disposable.dispose()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+ 
+}
+
+extension ConversationViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = conversationTable.dequeueReusableCell(withIdentifier: "conversationCell") as! ConversationViewCell
+        cell.conversation = conversationList?[indexPath.row]
+        return cell
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return conversationList!.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "Conversation", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "ChatViewController") as! UINavigationController
+        let chatVC = controller.viewControllers.first as! ChatViewController
+        chatVC.conversation = self.conversationList?[indexPath.row]
+        
+        self.present(controller, animated: true, completion: nil)
+    }
 
 }
